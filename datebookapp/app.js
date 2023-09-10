b0ae3743d1c20e196df187dc165cdb76ffb62e6b
xClaudiaArias <xClaudiaArias@gmail.com>
2023-09-10 12:48:04 -0400

    Login and Registration boilers

--- datebookapp/app.js
+++ datebookapp/app.js
@@ -54,25 +54,25 @@ app.use(function(err, req, res, next) {
 });
 
 // -------
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./db/datebook.db')
+// const sqlite3 = require('sqlite3').verbose()
+// const db = new sqlite3.Database('./db/datebook.db')
 
-db.serialize(() => {
-  db.run('CREATE TABLE lorem (info TEXT)')
-  const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+// db.serialize(() => {
+//   db.run('CREATE TABLE lorem (info TEXT)')
+//   const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
 
-  for (let i = 0; i < 10; i++) {
-    stmt.run(`Ipsum ${i}`)
-  }
+//   for (let i = 0; i < 10; i++) {
+//     stmt.run(`Ipsum ${i}`)
+//   }
 
-  stmt.finalize()
+//   stmt.finalize()
 
-  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-    console.log(`${row.id}: ${row.info}`)
-  })
-})
+//   db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+//     console.log(`${row.id}: ${row.info}`)
+//   })
+// })
 
-db.close()
+// db.close()
 
 // -------
 

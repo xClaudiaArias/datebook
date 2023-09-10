cc435b1de6ac5f3b1fd424472e0d58c84c22476b
xClaudiaArias <xClaudiaArias@gmail.com>
2023-09-09 23:56:27 -0400

    added sqlite "dummy" database

--- datebookapp/app.js
+++ datebookapp/app.js
@@ -53,6 +53,32 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// -------
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./db/datebook.db')
+
+db.serialize(() => {
+  db.run('CREATE TABLE lorem (info TEXT)')
+  const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+
+  for (let i = 0; i < 10; i++) {
+    stmt.run(`Ipsum ${i}`)
+  }
+
+  stmt.finalize()
+
+  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+    console.log(`${row.id}: ${row.info}`)
+  })
+})
+
+db.close()
+
+// -------
+
+
+
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 })
